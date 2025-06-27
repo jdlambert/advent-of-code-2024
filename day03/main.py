faa0167caaed861d5971672b564cae755bf30b8e
Jacob Lambert <jacob.lambert@gmail.com>
2025-06-27 15:51:06 -0700

    Finish day3

--- day03/main.py
+++ day03/main.py
@@ -4,11 +4,26 @@ import re
 with open("example.txt" if "x" in sys.argv else "input.txt") as f:
     data = f.read()
 
-mul = re.compile("mul\((\d+),(\d+)\)")
-muls = mul.findall(data)
+pattern = re.compile(
+    r"(?P<mul>mul\(\s*(?P<n>\d+)\s*,\s*(?P<m>\d+)\s*\))"
+    r"|(?P<do>do\(\))"
+    r"|(?P<dont>don't\(\))"
+)
 
-p1 = sum(int(mul[0]) * int(mul[1]) for mul in muls)
-p2 = 0
+muls = []
+do = True
+
+for match in pattern.finditer(data):
+    match_type = match.lastgroup
+    if match_type == "mul":
+        n = int(match.group('n'))
+        m = int(match.group('m'))
+        muls.append((n, m, do))
+    else:
+        do = match_type == "do"
+
+p1 = sum(n * m for m, n, _ in muls)
+p2 = sum(n * m for m, n, d in muls if d)
 
 print(f"Part one: {p1}")
 print(f"Part two: {p2}")
